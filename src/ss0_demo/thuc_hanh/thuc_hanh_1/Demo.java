3496a940ed9560c804fa3c88f58850cd1d7ff3cc
phanan1608 <96655589+phanan1608@users.noreply.github.com>
2022-02-16 23:43:49 +0700

    update thuc hanh ss2

--- src/ss0_demo/thuc_hanh/thuc_hanh_1/Demo.java
+++ src/ss0_demo/thuc_hanh/thuc_hanh_1/Demo.java
@@ -3,8 +3,6 @@ package ss0_demo.thuc_hanh.thuc_hanh_1;
 public class Demo {
     public static void main(String[] args) {
 
-        System.out.print('A');
-
     }
-    
+
 }
---
+++ src/ss2/thuc_hanh/thuc_hanh_1_is_prime/IsPrime.java
@@ -0,0 +1,28 @@
+package ss2.thuc_hanh.thuc_hanh_1_is_prime;
+
+import java.util.Scanner;
+
+public class IsPrime {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int number = scanner.nextInt();
+        boolean isPrime = true;
+        if (number < 2) {
+            isPrime = false;
+        } else {
+            int i = 2;
+            while (i <= Math.sqrt(number)) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+                i++;
+            }
+        }
+        if (isPrime)
+            System.out.println(number + " is a prime");
+        else
+            System.out.println(number + " is NOT a prime");
+    }
+}
