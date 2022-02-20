97c16557c57b0990a8e0c8ceb76b3b15275421ba
phanan1608 <96655589+phanan1608@users.noreply.github.com>
2022-02-20 21:58:19 +0700

    update bai tap ss4

--- src/ss0_demo/thuc_hanh/thuc_hanh_1/Demo.java
+++ src/ss0_demo/thuc_hanh/thuc_hanh_1/Demo.java
@@ -1,6 +1,9 @@
 package ss0_demo.thuc_hanh.thuc_hanh_1;
 
 public class Demo {
-    
+    public static void main(String[] args) {
+        int[] a = {1, 5, 10, -2};
+       
+    }
 
 }
---
+++ src/ss4_class_object_java/bai_tap/bai_tap_1_quadratic_equation/QuadraticEquation.java
@@ -0,0 +1,68 @@
+package ss4_class_object_java.bai_tap.bai_tap_1_quadratic_equation;
+
+import java.util.Scanner;
+
+public class QuadraticEquation {
+    double a, b, c;
+
+    private QuadraticEquation(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public double getDiscriminant() {
+        return Math.pow(b, 2) - 4 * a * c;
+    }
+
+    public double getRoot1() {
+        if (getDiscriminant() >= 0) {
+            return (-b + Math.pow(this.getDiscriminant(), 0.5)) / 2 * a;
+        } else {
+            return 0;
+        }
+    }
+
+    public double getRoot2() {
+        if (getDiscriminant() >= 0) {
+            return (-b - Math.pow(this.getDiscriminant(), 0.5)) / 2 * a;
+        } else {
+            return 0;
+        }
+    }
+
+    public String display() {
+        if (getRoot1() == 0 && getRoot2() == 0) {
+            return "The equation has no real roots";
+        } else if (getRoot1() == getRoot2()) {
+            return "The equation has one root " + this.getRoot1();
+        } else {
+            return "The equation has two roots " + this.getRoot1() + " and " + this.getRoot2();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a");
+        double a = scanner.nextDouble();
+        System.out.println("Enter b");
+        double b = scanner.nextDouble();
+        System.out.println("Enter c");
+        double c = scanner.nextDouble();
+        QuadraticEquation quadraticEquation = new QuadraticEquation(a, b, c);
+        System.out.println(quadraticEquation.display());
+        System.out.println(quadraticEquation.getA());
+    }
+}
