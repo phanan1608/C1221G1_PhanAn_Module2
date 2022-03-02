356d3c1cb3aa0425984290ac617a9dc97b27c1c1
phanan1608 <96655589+phanan1608@users.noreply.github.com>
2022-03-02 19:24:36 +0700

    update excercise ss12

---
+++ src/ss12_java_collection_framework/exercise/excercise_1_arraylist_linkedlist_in_java_collection/SortDown.java
@@ -0,0 +1,11 @@
+package ss12_java_collection_framework.exercise.excercise_1_arraylist_linkedlist_in_java_collection;
+
+
+import java.util.Comparator;
+
+public class SortDown implements Comparator<Product> {
+    @Override
+    public int compare(Product o1, Product o2) {
+        return (int) (o2.getPrice() - o1.getPrice());
+    }
+}
